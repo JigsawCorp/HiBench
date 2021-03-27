1438458b19e16e500e0f00b9d88c2a055aa68e12
Philippe Côté-Morneault <philbond1997@hotmail.com>
2021-03-27 12:54:59 -0400

    Fixed missing wildcard in hadoop examples paths.

--- bin/functions/load_config.py
+++ bin/functions/load_config.py
@@ -394,9 +394,9 @@ def probe_hadoop_examples_jars():
         examples_jars_candidate_cdh1 = HibenchConf[
             'hibench.hadoop.home'] + "/../../jars/hadoop-mapreduce-examples-*.jar"
         examples_jars_candidate_hdp0 = HibenchConf[
-            'hibench.hadoop.home'] + "/../hadoop-mapreduce-client/hadoop-mapreduce-examples.jar"
+            'hibench.hadoop.home'] + "/../hadoop-mapreduce-client/hadoop-mapreduce-examples-*.jar"
         examples_jars_candidate_hdp1 = HibenchConf[
-            'hibench.hadoop.home'] + "/../hadoop-mapreduce/hadoop-mapreduce-examples.jar"
+            'hibench.hadoop.home'] + "/../hadoop-mapreduce/hadoop-mapreduce-examples-*.jar"
 
         examples_jars_candidate_list = [
             examples_jars_candidate_apache0,
@@ -421,9 +421,9 @@ def probe_hadoop_examples_test_jars():
         examples_test_jars_candidate_cdh1 = HibenchConf[
             'hibench.hadoop.home'] + "/../../jars/hadoop-mapreduce-client-jobclient*-tests.jar"
         examples_test_jars_candidate_hdp0 = HibenchConf[
-            'hibench.hadoop.home'] + "/../hadoop-mapreduce-client/hadoop-mapreduce-client-jobclient-tests.jar"
+            'hibench.hadoop.home'] + "/../hadoop-mapreduce-client/hadoop-mapreduce-client-jobclient*-tests.jar"
         examples_test_jars_candidate_hdp1 = HibenchConf[
-            'hibench.hadoop.home'] + "/../hadoop-mapreduce/hadoop-mapreduce-client-jobclient-tests.jar"
+            'hibench.hadoop.home'] + "/../hadoop-mapreduce/hadoop-mapreduce-client-jobclient*-tests.jar"
 
         examples_test_jars_candidate_list = [
             examples_test_jars_candidate_apache0,
